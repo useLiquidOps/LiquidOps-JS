24b527aa04af0990bc80b3fa1af127eff52215bf
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2024-10-16 18:59:56 +0100

    chore: add mock token addresses

--- src/ao/tokenAddressData.ts
+++ src/ao/tokenAddressData.ts
@@ -1,21 +1,21 @@
 export type SupportedTokensTickers = "wAR" | "stETH";
 
-export type SupportedTokensAddresses = "123" | "456";
+export type SupportedTokensAddresses = "mmnvTHBi6gRWhY_Wtz08oxGq1DEtzkPKKdzNAi9XyPo" | "psh5nUh3VF22Pr8LeoV1K2blRNOOnoVH0BbZ85yRick";
 
 export const tokens: Record<SupportedTokensTickers, SupportedTokensAddresses> =
   {
-    wAR: "123",
-    stETH: "456",
+    wAR: "mmnvTHBi6gRWhY_Wtz08oxGq1DEtzkPKKdzNAi9XyPo",
+    stETH: "psh5nUh3VF22Pr8LeoV1K2blRNOOnoVH0BbZ85yRick",
   };
 
 export type SupportedOTokensTickers = "owAR" | "ostETH";
 
-export type SupportedOTokensAddresses = "123" | "456";
+export type SupportedOTokensAddresses = "2bCBzCi3Tr4RXAIumBdesr0IcDCBhfxFU8RXbO98c04" | "EO54_9d715SEqSXxx-nNsXqMyRgqyrYNIqb_OXSxSXA";
 
 export const oTokens: Record<
   SupportedOTokensTickers,
   SupportedOTokensAddresses
 > = {
-  owAR: "123",
-  ostETH: "456",
+  owAR: "2bCBzCi3Tr4RXAIumBdesr0IcDCBhfxFU8RXbO98c04",
+  ostETH: "EO54_9d715SEqSXxx-nNsXqMyRgqyrYNIqb_OXSxSXA",
 };
