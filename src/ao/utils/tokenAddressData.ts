9bd6e7c6331886dfe460c89669ba67f3e771de1b
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-01-23 15:51:02 +0000

    chore: new token data in SDK

--- src/ao/utils/tokenAddressData.ts
+++ src/ao/utils/tokenAddressData.ts
@@ -1,4 +1,6 @@
 export interface TokenData {
+  name: string;
+  icon: string;
   ticker: string;
   address: string;
   oTicker: string;
@@ -10,6 +12,8 @@ const controllerAddress = "vYlv6Dx8ZGt4oGaqsXaPjh9qi8iS8eQsoU9Ai65km4A";
 
 export const tokenData: Record<string, TokenData> = {
   QAR: {
+    name: "Quantum Arweave",
+    icon: "8VLMb0c9NATl4iczfwpMDe1Eh8kFWIUpSlIkcGfDFzM",
     ticker: "QAR",
     address: "XJYGT9ZrVdzQ5d7FzptIsKrJtEF4jWPbgC91bXuBAwU",
     oTicker: "oQAR",
@@ -17,6 +21,8 @@ export const tokenData: Record<string, TokenData> = {
     controllerAddress,
   },
   STETH: {
+    name: "Staked Ethereum",
+    icon: "UHjR0nY25BJ61LVSugyXCSoig07jv84LA6Wp_kYRfYI",
     ticker: "STETH",
     address: "GUJI7zjPoJ0uAHIBWiYrKL2bpwfltTZFXNL4J-IV8AI",
     oTicker: "oSTETH",
@@ -24,6 +30,8 @@ export const tokenData: Record<string, TokenData> = {
     controllerAddress,
   },
   USDC: {
+    name: "USD Circle",
+    icon: "iNYk0bDqUiH0eLT2rbYjYAI5i126R4ye8iAZb55IaIM",
     ticker: "USDC",
     address: "EoGGnxiSIUr0C5aZhTA_c8WipuASleomMrvyvuJCKvM",
     oTicker: "oUSDC",
