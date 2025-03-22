059f6ec9337aafd119767680931955148b5773c4
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-03-22 09:59:01 +0000

    chore: test aotokens new import

--- src/functions/utils/getBalance.ts
+++ src/functions/utils/getBalance.ts
@@ -1,4 +1,4 @@
-import { Token, Quantity } from "ao-tokens";
+import * as aoTokens from "ao-tokens";
 
 export interface GetBalance {
   tokenAddress: string;
@@ -8,15 +8,15 @@ export interface GetBalance {
 export async function getBalance({
   tokenAddress,
   walletAddress,
-}: GetBalance): Promise<Quantity> {
+}: GetBalance): Promise<aoTokens.Quantity> {
   if (!tokenAddress || !walletAddress) {
     throw new Error("Please specify a tokenAddress and walletAddress.");
   }
 
   try {
-    const tokenInstance = await Token(tokenAddress);
+    const tokenInstance = await aoTokens.Token(tokenAddress);
     const balance = await tokenInstance.getBalance(walletAddress);
-    return new Quantity(balance.raw, tokenInstance.info.Denomination);
+    return new aoTokens.Quantity(balance.raw, tokenInstance.info.Denomination);
   } catch (error) {
     throw new Error("Error getting balance: " + error);
   }
