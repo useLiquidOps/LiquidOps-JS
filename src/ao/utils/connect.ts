dceb8ea57c518307925b1c9e1250cc83ef7d9108
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-05-19 15:12:56 +0100

    feat: fix connect undef error

--- src/ao/utils/connect.ts
+++ src/ao/utils/connect.ts
@@ -1,8 +1,48 @@
-import { connect } from "@permaweb/aoconnect";
 import { SpawnProcess } from "@permaweb/aoconnect/dist/lib/spawn";
 import { SendMessage } from "@permaweb/aoconnect/dist/lib/message";
 import { ReadResult } from "@permaweb/aoconnect/dist/lib/result";
 import { Types as AoConnectTypes } from "@permaweb/aoconnect/dist/dal";
+import * as aoConnect from "@permaweb/aoconnect";
+
+const DEFAULT_SERVICES: Services = {
+  MODE: "legacy",
+  MU_URL: "https://mu.ao-testnet.xyz",
+  CU_URL: "https://cu.ao-testnet.xyz",
+  GATEWAY_URL: "https://arweave.net",
+};
+
+export function connectToAO(services?: Partial<Services>) {
+  const {
+    MODE = DEFAULT_SERVICES.MODE,
+    GRAPHQL_URL,
+    GRAPHQL_MAX_RETRIES,
+    GRAPHQL_RETRY_BACKOFF,
+    GATEWAY_URL = DEFAULT_SERVICES.GATEWAY_URL,
+    MU_URL = DEFAULT_SERVICES.MU_URL,
+    CU_URL = DEFAULT_SERVICES.CU_URL,
+  } = services || {};
+
+  const { spawn, message, result } = aoConnect.connect({
+    // @ts-ignore, MODE is needed here but is not in the aoconnect type yet
+    MODE,
+    GATEWAY_URL,
+    GRAPHQL_URL,
+    GRAPHQL_MAX_RETRIES,
+    GRAPHQL_RETRY_BACKOFF,
+    MU_URL,
+    CU_URL,
+  });
+
+  return { spawn, message, result };
+}
+
+export interface AoUtils {
+  spawn: SpawnProcess;
+  message: SendMessage;
+  result: ReadResult;
+  signer: AoConnectTypes["signer"];
+  configs: Services;
+}
 
 export type Services = {
   /**
@@ -34,43 +74,3 @@ export type Services = {
    */
   CU_URL?: string;
 };
-
-const DEFAULT_SERVICES: Services = {
-  MODE: "legacy",
-  MU_URL: "https://mu.ao-testnet.xyz",
-  CU_URL: "https://cu.ao-testnet.xyz",
-  GATEWAY_URL: "https://arweave.net",
-};
-
-export function connectToAO(services?: Partial<Services>) {
-  const {
-    MODE = DEFAULT_SERVICES.MODE,
-    GRAPHQL_URL,
-    GRAPHQL_MAX_RETRIES,
-    GRAPHQL_RETRY_BACKOFF,
-    GATEWAY_URL = DEFAULT_SERVICES.GATEWAY_URL,
-    MU_URL = DEFAULT_SERVICES.MU_URL,
-    CU_URL = DEFAULT_SERVICES.CU_URL,
-  } = services || {};
-
-  const { spawn, message, result } = connect({
-    // @ts-ignore, MODE is needed here but is not in the aoconnect type yet
-    MODE,
-    GATEWAY_URL,
-    GRAPHQL_URL,
-    GRAPHQL_MAX_RETRIES,
-    GRAPHQL_RETRY_BACKOFF,
-    MU_URL,
-    CU_URL,
-  });
-
-  return { spawn, message, result };
-}
-
-export interface AoUtils {
-  spawn: SpawnProcess;
-  message: SendMessage;
-  result: ReadResult;
-  signer: AoConnectTypes["signer"];
-  configs: Services;
-}
