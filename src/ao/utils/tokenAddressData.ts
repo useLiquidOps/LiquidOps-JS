71586d5ad4d0d5252955ccaab27464e1bcec8fab
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-07-17 00:05:38 +0200

    chore: revert

--- src/ao/utils/tokenAddressData.ts
+++ src/ao/utils/tokenAddressData.ts
@@ -12,7 +12,6 @@ export interface TokenData {
   baseDenomination: bigint;
   deprecated: boolean;
   oIcon: string;
-  frozen: boolean;
 }
 
 export const controllerAddress = "SmmMv0rJwfIDVM3RvY2-P729JFYwhdGSeGo2deynbfY";
@@ -35,7 +34,6 @@ export const tokenData: Record<string, TokenData> = {
     baseDenomination: BigInt(12),
     deprecated: true,
     oIcon: "i_U-jhdMMaib2hK51qPrKXbLo6cx2Nt58_gNz5FA4sw",
-    frozen: true
   },
   WAR: {
     name: "Wrapped Arweave",
@@ -51,7 +49,6 @@ export const tokenData: Record<string, TokenData> = {
     baseDenomination: BigInt(12),
     deprecated: false,
     oIcon: "lTWBOBtEZ2JvTAHfvoPq5aXRWTVouv7jZ-6B9HTwosU",
-    frozen: false
   },
   WUSDC: {
     name: "Wrapped USD Circle",
@@ -67,7 +64,6 @@ export const tokenData: Record<string, TokenData> = {
     baseDenomination: BigInt(6),
     deprecated: false,
     oIcon: "7EEISJIzxC-3RPhgvRc-lAZnP7st1b79_ER4Sc5P_MU",
-    frozen: false
   },
   WUSDT: {
     name: "Wrapped USD Tether",
@@ -83,7 +79,6 @@ export const tokenData: Record<string, TokenData> = {
     baseDenomination: BigInt(18),
     deprecated: false,
     oIcon: "bkAnKOF4NhqPHnccDhPyOzBws42zNE-u9WtxCPdaABU",
-    frozen: false
   },
   WETH: {
     name: "Wrapped Ethereum",
@@ -99,7 +94,6 @@ export const tokenData: Record<string, TokenData> = {
     baseDenomination: BigInt(18),
     deprecated: false,
     oIcon: "z1nnBgzGpt-eXHrjD5A9KrQX6dK8E1ONDuBIqB94VTA",
-    frozen: false
   },
 };
 
